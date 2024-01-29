844e93b9e509ee667aa43da6c1fac65cc7b06c91
acantu9 <adrianaacantu@gmail.com>
2024-01-28 23:18:58 -0600

    updated to include links to my github, linked in and stack overflow profiles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-function Footer() {
- return (
+const Footer = () => {
+  return (
     <footer>
-      {/* Add your desired footer content here */}
+      <p>
+        &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+      </p>
+      <ul>
+        <li>
+          <a href="https://github.com/acantu9" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/adriana-cant%C3%BA-2243392b1/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://stackoverflow.com/users/22900778/adriana-cant%c3%ba" target="_blank" rel="noopener noreferrer">
+            Stack Overflow
+          </a>
+        </li>
+      </ul>
     </footer>
- );
-}
+  );
+};
 
 export default Footer;
