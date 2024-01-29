63fd76a1e2c83890900d342daccf9a45bf20af8a
acantu9 <adrianaacantu@gmail.com>
2024-01-28 23:04:32 -0600

    defined the portfolio data

---
+++ src/data/portfolioData.js
@@ -0,0 +1,18 @@
+const portfolioData = [
+    {
+      id: 1,
+      title: "Bar Crawler",
+      image: "../assets/img/barcrawler.png",
+      deployedLink: "https://acantu9.github.io/team-5-project/",
+      githubLink: "https://github.com/acantu9/team-5-project.git",
+    },
+    {
+      id: 2,
+      title: "DEVJOBS",
+      image: "../assets/img/barcrawler.png",
+      deployedLink: "https://shielded-hollows-32035-7796c32f6e6c.herokuapp.com/",
+      githubLink: "https://github.com/lildano50/DevJobs.git",
+    },
+];
+  
+export default portfolioData;
